cc44e766193a93dfd2261cc376d1f9af135e9da4
sentes <sentes123@gmail.com>
2026-08-24 16:46:05 +0200

    snooze icon

--- Phone/app/src/main/java/com/sentes/bluereminder/ui/SnoozeDropdown.kt
+++ Phone/app/src/main/java/com/sentes/bluereminder/ui/SnoozeDropdown.kt
@@ -1,10 +1,13 @@
 package com.sentes.bluereminder.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Snooze
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,8 +20,8 @@ fun SnoozeDropdown(onSnoozeSelected: (Long) -> Unit) {
     var expanded by remember { mutableStateOf(false) }
 
     Box {
-        TextButton(onClick = { expanded = true }) {
-            Text("+...")
+        IconButton(onClick = { expanded = true }) {
+            Icon(Icons.Default.Snooze, contentDescription = "Drzemka")
         }
         DropdownMenu(
             expanded = expanded,
